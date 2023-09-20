edc0e2cdec7ed51e1643b3df35e8e8c1db41e8f3
abhishek-Kumar009 <wizzard.coder@gmail.com>
2023-09-21 03:33:05 +0530

    pool create

--- tasks/createPool.ts
+++ tasks/createPool.ts
@@ -2,6 +2,9 @@ import { getAddress } from "@zetachain/protocol-contracts";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+import UniswapV2FactoryAbi from "../abis/uniswapV2Factory.json";
+import { EddyBNB, EddyBTC, EddyETH, EddyMATIC } from "../utils/common";
+
 const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
   if (hre.network.name !== "zeta_testnet") {
     throw new Error(
@@ -9,17 +12,32 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     );
   }
 
+  const tokenA = EddyBTC;
+  const tokenB = EddyETH;
+
   const [signer] = await hre.ethers.getSigners();
   console.log(`🔑 Using account: ${signer.address}\n`);
 
   // Get uniswapV2 factory contract to create pool
-  const uniswapV2FactoryAddressZetachain = '0x9fd96203f7b22bCF72d9DCb40ff98302376cE09c';
-  
+  const uniswapV2FactoryAddressZetachain =
+    "0x9fd96203f7b22bCF72d9DCb40ff98302376cE09c";
 
-//   console.log(`🚀 Successfully deployed WAVE token contract on ZetaChain.
-// 📜 Contract address: ${contract.address}
-// 🌍 Explorer: https://athens3.explorer.zetachain.com/address/${contract.address}
-// `);
+  const uniswapV2FactoryContract = new hre.ethers.Contract(
+    uniswapV2FactoryAddressZetachain,
+    UniswapV2FactoryAbi,
+    signer
+  );
+
+  const tx = await uniswapV2FactoryContract.createPair(tokenA, tokenB);
+
+  tx.wait();
+
+  const hash = tx.hash;
+
+  console.log(`🚀 Successfully created pool.
+  📜 Hash of the transaction: ${hash}
+  🌍 Explorer: https://explorer.zetachain.com/evm/tx/${hash}
+  `);
 };
 
 task("createPool", "Creating pool for tokens", main);
