9b105abaa095c45c53e5b21c0120f4ce3d375240
abhishek-Kumar009 <wizzard.coder@gmail.com>
2023-10-30 19:32:43 +0530

    EVM -> BTC bu

--- contracts/ZetaSwapV2.sol
+++ contracts/ZetaSwapV2.sol
@@ -8,29 +8,56 @@ import "@zetachain/toolkit/contracts/SwapHelperLib.sol";
 
 contract ZetaSwapV2 is zContract {
     error SenderNotSystemContract();
+    error WrongAmount();
 
     SystemContract public immutable systemContract;
 
+    // Testnet BTC(Zeth)
+    address public immutable BTC_ZETH = 0x65a45c57636f9BcCeD4fe193A602008578BcA90b;
+
     constructor(address systemContractAddress) {
         systemContract = SystemContract(systemContractAddress);
+    }
+
+    function bytesToBech32Bytes(
+        bytes calldata data,
+        uint256 offset
+    ) internal pure returns (bytes memory) {
+        bytes memory bech32Bytes = new bytes(42);
+        for (uint i = 0; i < 42; i++) {
+            bech32Bytes[i] = data[i + offset];
+        }
+
+        return bech32Bytes;
+    }
+
+    function _getTargetRecipientForBTCWithdrawal (bytes calldata message) internal pure returns(bytes memory) {
+        return bytesToBech32Bytes(message, 20);
     }
 
     function _getTargetAndRecipient (
         bytes calldata message
     ) internal pure returns(address targetZRC20BTC, bytes32 recipientBTC) {
         targetZRC20BTC = BytesHelperLib.bytesToAddress(message, 0);
-
         address recipientAddr = BytesHelperLib.bytesToAddress(message, 20);
         recipientBTC = BytesHelperLib.addressToBytes(recipientAddr);
     }
 
-    function _swapAndWithdraw(
+    function getTargetOnly (bytes calldata message) internal pure returns(address targetChain) {
+        return BytesHelperLib.bytesToAddress(message, 0);
+    }
+
+    function getRecipientOnly(bytes calldata message) internal pure returns (bytes32 recipientBTC) {
+        address recipientAddr = BytesHelperLib.bytesToAddress(message, 20);
+        recipientBTC = BytesHelperLib.addressToBytes(recipientAddr);
+    }
+
+    function _swap(
         address _zrc20,
         uint256 _amount,
         address _targetZRC20,
-        uint256 _minAmountOut,
-        bytes32 _recipient
-    ) internal {
+        uint256 _minAmountOut
+    ) internal returns (uint256){
 
         uint256 outputAmount = SwapHelperLib._doSwap(
             systemContract.wZetaContractAddress(),
@@ -41,7 +68,8 @@ contract ZetaSwapV2 is zContract {
             _targetZRC20,
             _minAmountOut
         );
-        SwapHelperLib._doWithdrawal(_targetZRC20, outputAmount, _recipient);
+        
+        return outputAmount;
 
     }
 
@@ -55,14 +83,31 @@ contract ZetaSwapV2 is zContract {
             revert SenderNotSystemContract();
         }
 
-        (address targetZRC20, bytes32 recipient) = _getTargetAndRecipient(message);
-            uint256 minAmt = 0;
-            _swapAndWithdraw(
+        address targetZRC20 = getTargetOnly(message);
+        uint256 minAmt = 0;
+
+         if (targetZRC20 == BTC_ZETH) {
+            bytes memory recipientAddressBech32 = bytesToBech32Bytes(message, 20);
+            uint256 outputAmount = _swap(
                 zrc20,
                 amount,
                 targetZRC20,
-                minAmt,
-                recipient
+                minAmt
             );
+            (, uint256 gasFee) = IZRC20(targetZRC20).withdrawGasFee();
+            IZRC20(targetZRC20).approve(targetZRC20, gasFee);
+            if (outputAmount < gasFee) revert WrongAmount();
+
+            IZRC20(targetZRC20).withdraw(recipientAddressBech32, outputAmount - gasFee);
+        } else {
+            bytes32 recipient = getRecipientOnly(message);
+            uint256 outputAmount = _swap(
+                zrc20,
+                amount,
+                targetZRC20,
+                minAmt
+            );
+            SwapHelperLib._doWithdrawal(targetZRC20, outputAmount, recipient);
+        }
     }
 }
